8ac6517c22b58238a37923ba4ce07d7eef94d284
unknown <sumair_ahmed4@yahoo.co.in>
2018-06-16 19:29:37 +1000

    Added TodoList component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="">
+    <input type="text" class="todo-input" placeholder="What needs to be done">
+    <div class="" v-for="(todo, index) in todos">
+      {{ todo.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'todo-list',
+  data() {
+    return {
+      todos: [
+        {
+          title: 'Start learning vuejs',
+          completed: false
+        },
+        {
+          title: 'Implement a simple vuejs app',
+          completed: false
+        }
+      ]
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
