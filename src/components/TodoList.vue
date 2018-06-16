46091837db9a8888d79b3cb447c7440c8f150355
unknown <sumair_ahmed4@yahoo.co.in>
2018-06-16 20:19:50 +1000

    Added button to delete todo items

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,12 @@
   <div class="">
     <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
     <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
-      {{ todo.title }}
+      <div class="">
+          {{ todo.title }}
+      </div>
+      <div class="remove-item" @click="removeTodo(index)">
+        &times;
+      </div>
     </div>
   </div>
 </template>
@@ -31,11 +36,20 @@ export default {
   },
   methods: {
     addTodo() {
+      if (this.newTodo.trim().length === 0) {
+        return
+      }
       this.todos.push({
         id: this.idForTodo,
         title: this.newTodo,
         completed: false
       })
+
+      this.newTodo = ''
+      this.idForTodo++
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
     }
   }
 }
@@ -48,8 +62,23 @@ export default {
   padding: 10px 18px;
   font-size: 18px;
   margin-bottom: 16px;
+
+  &:focus {
+    outline: 0;
+  }
 }
 .todo-item {
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.remove-item {
+  cursor: pointer;
+  margin-left: 14px;
 
+  &:focus {
+    color: black;
+  }
 }
 </style>
