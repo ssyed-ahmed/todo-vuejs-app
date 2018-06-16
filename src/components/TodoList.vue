82a1650b8ec6e0a5764b878e602269ed3da752fc
unknown <sumair_ahmed4@yahoo.co.in>
2018-06-16 19:55:20 +1000

    Modified files to include input text box for new todo item

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
-    <input type="text" class="todo-input" placeholder="What needs to be done">
-    <div class="" v-for="(todo, index) in todos">
+    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
       {{ todo.title }}
     </div>
   </div>
@@ -13,16 +13,29 @@ export default {
   name: 'todo-list',
   data() {
     return {
+      newTodo: '',
+      idForTodo: 3,
       todos: [
         {
+          id: 1,
           title: 'Start learning vuejs',
           completed: false
         },
         {
+          id: 2,
           title: 'Implement a simple vuejs app',
           completed: false
         }
       ]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false
+      })
     }
   }
 }
@@ -30,5 +43,13 @@ export default {
 </script>
 
 <style lang="scss">
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+}
+.todo-item {
 
+}
 </style>
